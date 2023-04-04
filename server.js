c25fe2c53188408946f93707599ef46dd59cf8aa
Melanie <mtojong@live.unc.edu.email>
2023-04-04 17:20:13 -0400

    restarting local database

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '!heartDante19',
+        database: 'pantry'
     }
 })
 
